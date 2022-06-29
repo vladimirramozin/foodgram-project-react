3538b860e403cb47bed7740721a61d2955d2cd90
vladimir <vladimirramozin@yandex.ru>
2022-06-29 20:25:14 +0800

    29/06

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 from rest_framework.authtoken import views
 
 urlpatterns = [
-   # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     #path('token/', views.obtain_auth_token),
     path('api/', include('api.urls')),
     
