69ddad0e4fdfbb63284219d08c6089b613945780
vladimir <vladimirramozin@yandex.ru>
2022-07-02 19:45:28 +0800

    02/07

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -57,6 +57,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
+     )
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
 }
--- backend/recipe/models.py
+++ backend/recipe/models.py
@@ -77,7 +77,7 @@ class Recipe(models.Model):
    )
     name = models.CharField(max_length=200, verbose_name='Назавание рецепта')
     text = models.TextField(verbose_name='Описание рецепта')
-    image = ImageField(upload_to = 'static')
+    image = models.ImageField(upload_to = 'static')
     ingredients = models.ManyToManyField(Ingredients, blank=True)
     tags = models.ManyToManyField(Tag, blank=True)
     cooking_time=models.PositiveIntegerField(validators=[MinValueValidator(1, 'out of range')], verbose_name = 'время приготовления в минутах')
