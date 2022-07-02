83a2a719d9801cf8e48399221a77a3e65cbdccab
Владимир Тихий <91606744+vladimirramozin@users.noreply.github.com>
2022-07-02 17:11:49 +0500

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -60,7 +60,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.FormParser',
         'rest_framework.parsers.MultiPartParser'
-     )
+     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
 }
