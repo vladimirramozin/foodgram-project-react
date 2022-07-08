cd738ba983da0972321329df67ac8786007800fe
Владимир Тихий <91606744+vladimirramozin@users.noreply.github.com>
2022-07-08 22:37:52 +0500

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -66,7 +66,7 @@ class IngredientsViewSet(viewsets.ReadOnlyModelViewSet):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    pagination_class=PageNumberPagination
+    #pagination_class=PageNumberPagination
     @action(detail=False, methods=['get'])
     def me(self, request):
         serializer = self.get_serializer(request.user)
@@ -77,7 +77,7 @@ class UserViewSet(viewsets.ModelViewSet):
         permission_classes=(IsAuthenticated,),
     )
     def subscriptions(self, request):
-        #pagination_class=PageNumberPagination
+        pagination_class=PageNumberPagination
         user=request.user
         subscriptions=Subscriptions.objects.filter(user=user).values_list('following_id', flat=True)
         subscriptions_users=User.objects.filter(id__in=subscriptions)
