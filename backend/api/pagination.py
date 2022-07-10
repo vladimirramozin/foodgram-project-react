2248b7248685e1b723bd3d33f992a3dd6e5ebe93
vladimir <vladimirramozin@yandex.ru>
2022-07-10 16:31:41 +0800

    10.07

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,8 +1,5 @@
 from rest_framework.pagination import PageNumberPagination
 
 class FoodPagination(PageNumberPagination):
-    page_size = 2
     page_size_query_param = 'limit'
-    max_page_size = 50
-    page_query_param = 'page'
-
+    page_size = 10
