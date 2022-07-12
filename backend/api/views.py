d20f32f891f853888f74b9323df5e7e50cb2614d
Владимир <vladimirramozin@yandex.ru>
2022-07-12 16:59:44 +0500

    12.07

--- backend/api/views.py
+++ backend/api/views.py
@@ -90,7 +90,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         result_cart = {}
         for obj in shopping_cart:
             recipe=obj.in_shopping_cart
-            ingredients=recipe.ingredients.ingredient
+            ingredients=recipe.ingredients__ingredient
             for ingredient in ingredients:
                 if result_cart[ingredient.ingredient.name].exists():
                     result_cart[ingredient.ingredient.name]+=ingredient.amount
