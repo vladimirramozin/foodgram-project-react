1a64dd540439345008fbc0f68c7d63ee7a9188ac
VladimirTihiy <109247990+VladimirTihiy@users.noreply.github.com>
2022-07-16 19:13:20 +0300

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.AllowAny', 
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
