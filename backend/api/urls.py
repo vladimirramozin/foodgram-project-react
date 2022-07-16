4f6b07dc3899c740f44c7432e9a335461aba3508
VladimirTihiy <109247990+VladimirTihiy@users.noreply.github.com>
2022-07-16 11:47:42 +0300

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -30,8 +30,8 @@ router.register('ingredients', IngredientsViewSet)
 
 
 urlpatterns = [
-    path('',  include(router.urls)),
     path('', include('djoser.urls')),
+    path('',  include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
 
 ]
