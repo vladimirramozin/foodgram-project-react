2f1ef7feb96d5131b448c8e73eba601c06ff3980
VladimirTihiy <109247990+VladimirTihiy@users.noreply.github.com>
2022-07-17 14:56:23 +0300

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -13,7 +13,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import (HTTP_200_OK, HTTP_201_CREATED,
-                                   HTTP_204_NO_CONTENT)
+                                   HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST)
 from users.models import User
 
 from .filters import RecipeFilter
@@ -173,7 +173,7 @@ class UserViewSet(viewsets.ModelViewSet):
         if serializer.is_valid():
             serializer.save()
             return Response(
-                status=status.HTTP_204_NO_CONTENT
+                status=HTTP_204_NO_CONTENT
             )
 
         return Response(
