f0d4eca81e97ce3dba47aecaa0ee5ad19ff66f65
VladimirTihiy <109247990+VladimirTihiy@users.noreply.github.com>
2022-07-17 11:25:04 +0300

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -20,7 +20,8 @@ from .views import IngredientsViewSet, RecipeViewSet, TagViewSet, UserViewSet
 
 router = DefaultRouter()
 
-router.register(r'users', UserViewSet, basename='users')
+router.register('users/subscriptions', UserViewSet)
+router.register('users/subscribe', UserViewSet)
 router.register('recipes', RecipeViewSet)
 router.register('tags', TagViewSet)
 router.register('ingredients', IngredientsViewSet)
