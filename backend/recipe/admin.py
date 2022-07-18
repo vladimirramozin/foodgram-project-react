3f5ed4eb5f8953ff959b9943984743af124b250e
VladimirTihiy <109247990+VladimirTihiy@users.noreply.github.com>
2022-07-18 18:52:43 +0300

    Update admin.py

--- backend/recipe/admin.py
+++ backend/recipe/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 from django.dispatch import receiver
-
+from django.contrib.admin import ModelAdmin, display, register
 from .models import (FavoriteRecipies, Ingredient, Ingredients, Recipe,
                      ShoppingCart, Subscriptions, Tag)
 
 admin.site.register(Subscriptions)
-admin.site.register(Recipe)
 admin.site.register(Ingredient)
 admin.site.register(Ingredients)
 admin.site.register(Tag)
 admin.site.register(FavoriteRecipies)
 admin.site.register(ShoppingCart)
+
+@register(Recipe)
+class RecipeAdmin(ModelAdmin):
+    list_display = ('id', 'name', 'author', 'tags',)
+    search_fields = ('name', 'author',)
+    readonly_fields = ('added_in_favorites',)
+
+    @display
+    def added_in_favorites(self, obj):
+        return obj.favorite.count()
